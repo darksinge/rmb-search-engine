a23e06701b235a6659b107017f45ce42a1d85e91
Bradley Robinson <bradley.s.robinson12@gmail.com>
2017-04-19 12:36:37 -0600

    Cleaned up unused code, added documentation

--- search.py
+++ search.py
@@ -4,8 +4,7 @@ Created on Tue Apr 11 08:32:10 2017
 
 @author: Bradley Robinson
 
-Code to search documents using csv, in the future it will use a SQL server
-so a similar algorithm can be called in javascript
+Searching algorithms for documents, along with some testing script to demonstrate the effectiveness of the algorithms.
 
 """
 import pandas as pd
@@ -60,17 +59,48 @@ class ClusterFinder(object):
 
 
 class TermSearch(object):
+    """
+    Object created and used for search algorithm, this allows a term search to occur without loading the search contents
+    into memory every time a search is done, which could be time consuming.
 
+    Methods:
+        tokenize:
+        _search_term: internal method to find sparse_matrix terms
+
+
+    Variables:
+        self.sparse_matrix: a pre-loaded sparse_matrix for searching that allows users to search for terms. The document
+        is a pickle that is created previously in create_sparse_matrix.py.
+    """
     def __init__(self):
+        # May be needed, for now we won't bother
         from collections import OrderedDict
         self.sparse_matrix = pickle.load(open(os.path.join('analysis', 'sparse_p.pickle'), 'rb'))
 
     def tokenize(self, text):
+        """
+        Strips non-numerical/alphabetical characters and splits up terms in the search.
+
+        Parameters:
+            text: string, search text to be tokenized
+
+        Returns:
+            list: strings of terms to be searched for
+        """
         stripped_text = re.sub('[^a-z0-9 *]', ' ', text.lower())
         terms = stripped_text.split()
         return terms
 
     def _search_term(self, term):
+        """
+        Internal method to find the given term in the dictionary and return results, if there are any.
+
+        Parameters:
+            term: a single string that is just one term.
+
+        Returns:
+            dict: a dictionary with the matching results
+        """
         try:
             doc_dict = self.sparse_matrix[term]
             return doc_dict
@@ -78,6 +108,17 @@ class TermSearch(object):
             return {}
 
     def combine_dicts(self, dict1, dict2):
+        """
+        Takes two dictionaries and combines them so that none of the documents are overridden. If two match between
+        documents, they are added together.
+
+        Parameters:
+            dict1: dictionary of documents for the first term with document as key and the tf_idf score as the value
+            dict2: same, but for the second term.
+
+        Returns:
+             dict: a dictionary with terms from both documents combined
+        """
         combined = dict2.copy()
         for k, terms in dict2.items():
             if k in dict1:
@@ -87,6 +128,14 @@ class TermSearch(object):
         return combined
 
     def search(self, text):
+        """
+        Method to search for documents that match the users needs.
+
+        Parameters:
+            text: a string of text with terms representing a user's interest.
+        Returns:
+            dict: dictionary with documents and their tf_idf score.
+        """
         terms = self.tokenize(text)
         current_dict = {}
         for i in range(len(terms)):
@@ -97,99 +146,17 @@ class TermSearch(object):
         return current_dict
 
 
-
-def check_for_terms(tokens, df_columns):
+def scan_similar_documents(lst, similar):
     """
-    Finds any terms that may be in the the dataframe. Should be called before
-    creating a tf-idf with any hits for the search terms.
-    
-    Args:
-        tokens - a Python list of tokenized terms to be searched for
-        df_columns - a list of columns (or terms) in the document database
+    Takes a list of files and prints a few of the lines from them to check and make sure the algorithm worked. Simply
+    used for validation of the ClusterFinder algorithms.
+
+    Parameters:
+        lst: list or similar iterable of strings, goes through each file and prints the contents
+        similar: string, name of the document that the listed documents represent
+
     Returns:
-        A list of all the terms that have hits
-    """
-    pass
-
-
-def csv_search(tokens, df):
-    """
-    Finds any hits in the CSV document. Note that in the future sql_search will
-    be used for this, which will be optimized for the large matrix
-    
-    How it will be done:
-        looks for each term in the columns
-        if there are none, return failure
-        Using the list for each hit, then create dataframes using OR operation
-        for any that have tf-idf > 0
-        
-        Find new scores
-        Order the documents
-        Return ordered list
-        
-    Args:
-        tokens: a Python list of tokenized terms to be searched for
-        
-    Returns:
-        A tf_idf ordered list for any hits
-    """
-    searchable_terms = check_for_terms(tokens, df)
-    if len(searchable_terms) == 0:
-        # TODO: exception
-        return None
-    
-    term_df = pd.DataFrame(columns=searchable_terms)
-    for term in searchable_terms:
-        with_term = df[df[term] > 0]
-        # term_df.append()
-        
-
-def tokenize(unfiltered_text):
-    """
-    Takes search text and removes any extra figures, etc., to allow for a 
-    search of the documents.
-    """
-    # TODO: Actually tokenize
-    return unfiltered_text
-    
-
-def find_text(text, search_type="csv", df=None):
-    """
-    Retrieves tf-idf ordered results in a list for document search.
-    bill_data_analysis.main() must be run if additional bill scraping has been
-    done to ensure new bills are included in the search.
-    
-    Args:
-        text: a string of search terms
-        search_type: a string representing how the search will be done. 
-        Accepted parameters are "csv" and "sql"
-        df - If search_type="csv", this parameter must include a tf-idf table
-        
-    Returns:
-        A tf-idf ordered list for any hits.
-    """
-    tokens = tokenize(text)
-    if search_type == "csv":
-        csv_search(tokens, df)
-    else:
-        # TODO: Throw exception
-        pass
-    return []
-
-
-def test_search():
-    """
-    Used for development
-    """
-    dense_matrix = pd.read_csv(os.path.join("analysis", "dense_matrix.csv"))
-    find_text(["bill", "rights", "taxes"], df=dense_matrix)
-
-
-def open_files(lst, similar):
-    """
-    Takes a list of files and prints a few of the lines from them to check and make sure the algorithm worked
-    :param lst:
-    :return:
+        None
     """
 
     for f in lst:
