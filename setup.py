a6d3297f4d29f9c4c4bfe5beab762032dacc2bc3
Bradley Robinson <bradley.s.robinson12@gmail.com>
2017-04-22 18:06:35 -0600

    Added a requirements.txt

---
+++ setup.py
@@ -0,0 +1,12 @@
+from distutils.core import setup
+
+setup(
+    name='IR-legislation-cs4320_sp17',
+    version='0.0.1',
+    packages=[''],
+    url='',
+    license='',
+    author='Bradley Robinson',
+    author_email='bradley.s.robinson12@gmail.com',
+    description=''
+)
